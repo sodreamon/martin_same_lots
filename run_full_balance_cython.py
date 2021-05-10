8b40004572aabb7e4fc0f5b16f8a65d4fa4fb253
sodreamon <>
2021-05-10 08:43:18 +0000

    cython setup & run file

---
+++ run_full_balance_cython.py
@@ -0,0 +1,3 @@
+import os
+os.system('python setup_full_bal.py build_ext --inplace')
+import full_balance_cython
---
+++ run_origin_cython.py
@@ -0,0 +1,3 @@
+import os
+os.system('python setup_origin.py build_ext --inplace')
+import origin_cython
